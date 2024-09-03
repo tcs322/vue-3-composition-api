16d07a535a442d57c5a9d55ae036e43b8f271491
Thyerre <thyerre.sampaio@kolaresti.com.br>
2024-09-03 16:12:01 -0300

    ]primeiro commit

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/todos',
+    name: 'todos.index',
+    component: () => import('../views/Todos/TodoView.vue')
   }
 ]
 
---
+++ src/views/Todos/TodoView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Lista de tarefas</h1>
+        <ul>
+            <li v-for="tarefa in tarefas" :key="tarefa.id">
+                {{ tarefa.name }}
+            </li>
+        </ul>
+        <input type="text" v-model="name">
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    name: 'TodoView',
+    setup () {
+        const tarefas = [
+            {id: 1, name: 'tarefa 01'},
+            {id: 2, name: 'tarefa 02'}
+        ]
+
+        const name = ref('Thyerre')
+    
+        return {
+            tarefas,
+            name
+        }
+    }
+}
+
+</script>
